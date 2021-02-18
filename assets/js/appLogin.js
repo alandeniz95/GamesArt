fe86fa61a1388a18ea310c94736d0850a93e0d4e
Alan Deniz <alandeniz95@hotmail.com>
2021-02-18 20:52:46 -0300

    Repositorio

---
+++ assets/js/appLogin.js
@@ -0,0 +1,24 @@
+document.getElementById('btn-iniciarSesion').addEventListener('click', iniciarSesion);
+document.getElementById('btn-registrar').addEventListener('click', registrar);
+
+const contenedorLoginRegistro = document.querySelector('.contenedor-login-registro');
+const formLogin = document.querySelector('.form-login');
+const formRegistro = document.querySelector('.form-registro');
+const cajaLogin = document.querySelector('.caja-trasera-login');
+const cajaRegistro = document.querySelector('.caja-trasera-registrar');
+
+function iniciarSesion(){
+  formRegistro.style.display = 'none';
+  contenedorLoginRegistro.style.left = '10px';
+  formLogin.style.display = 'block';
+  cajaRegistro.style.opacity = '1';
+  cajaLogin.style.opacity = '0';
+}
+
+function registrar(){
+  formRegistro.style.display = 'block';
+  contenedorLoginRegistro.style.left = '410px';
+  formLogin.style.display = 'none';
+  cajaRegistro.style.opacity = '0';
+  cajaLogin.style.opacity = '1';
+}
---
+++ assets/js/appPS.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () =>{
+  fetchData()
+});
+const fetchData = async () =>{
+  try {
+    const res = await fetch('../data/dataBasePS.json');
+    const data = await res.json();
+    //console.log(data);
+    pintarProductos(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const contenedorProductos = document.querySelector('#contenedor-productos');
+const pintarProductos = (data) =>{
+  const template = document.querySelector('#template-productos').content
+  const fragment = document.createDocumentFragment();
+  //console.log(template);
+  data.forEach(producto => {
+    //console.log(producto);
+    template.querySelector('img').setAttribute('src', producto.img);
+    template.querySelector('h5').textContent = producto.tittle;
+    template.querySelector('h6').textContent = producto.marca;
+    template.querySelector('p span').textContent = producto.precio;
+    template.querySelector('button').dataset.id = producto.id;
+
+
+    const clone = template.cloneNode(true);
+    fragment.appendChild(clone);   
+  });
+  contenedorProductos.appendChild(fragment);
+}
